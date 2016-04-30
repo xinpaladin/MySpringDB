0d4bad84ecd2c4c27e0c41b82727cf0b5d189724
xinpaladin <693914175@qq.com>
2016-04-30 21:41:11 +0800

    修改配置文件

--- src/main/java/cn/xjtu/locus/Controller/IndexController.java
+++ src/main/java/cn/xjtu/locus/Controller/IndexController.java
@@ -1,23 +1,10 @@
 package cn.xjtu.locus.Controller;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedList;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import cn.xjtu.locus.Dao.DataDetailDao;
-import cn.xjtu.locus.Dao.DataItemDao;
-import cn.xjtu.locus.Entity.DataDetail;
-import cn.xjtu.locus.Entity.DataItem;
 import cn.xjtu.locus.Service.DataDetailService;
 import cn.xjtu.locus.Service.DataItemService;
 import cn.xjtu.locus.common.Result;
